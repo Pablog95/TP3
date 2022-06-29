8f48063e1b30b9165d8531da0ebd9b2ef7992fc3
Magaliburstein <106644403+Magaliburstein@users.noreply.github.com>
2022-06-29 13:26:10 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,26 @@
 #include "menu.h"
 
 int main(){
-   
-   Lector* escritor = new Lector;
+
+    //Cola<Lectura> colaLectura;
+    Hashing<Escritor*>* tabla_escritor;
+    Lector* escritor = new Lector(tabla_escritor);
+
 
    escritor->lectura_archivo_escritores();
    escritor->leer_lectura();
 
    Lista<Escritor*>* escritores = escritor->obtener_Lescritores();
    Lista<Lectura*>* lecturas = escritor->obtener_Llecturas();
-   
+
    //escritores->listar();
    //lecturas->listar();
+
+   //Archivo a1(& escritor, & lecturas , & colaLectura, & tablaEscritor);
+   //Menu m1(& escritor, & lecturas, &colaLectura, & tablaEscritor);
   
 
-   Menu* menu = new Menu(lecturas, escritores);
+   Menu* menu = new Menu(lecturas, escritores, tabla_escritor);
    menu->implementacion_menu();
 
    lecturas->destruir();
